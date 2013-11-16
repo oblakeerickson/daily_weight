f5f07aa35942e7df388dc715b7807470ed2e0476
Blake Erickson <o.blakeerickson@gmail.com>
2013-11-16 07:31:47 -0700

    added styling and fixed the routes so that the home page is the root url

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,13 +5,18 @@ describe "StaticPages" do
   describe "Home page" do
 
     it "should have the content 'Daily Weight'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Daily Weight')
     end
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Daily Weight | Home")
+    it "should have the title" do
+      visit root_path
+      expect(page).to have_title("Daily Weight")
+    end
+
+    it "should not have a custom page title" do
+      visit root_path
+      expect(page).not_to have_title(' | Home')
     end
   end
 end
