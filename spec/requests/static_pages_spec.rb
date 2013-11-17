4c63f02989d517d2a6a860d41f4b9f62637b17d5
Blake Erickson <o.blakeerickson@gmail.com>
2013-11-17 06:25:18 -0700

    created sign-up page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,21 +2,13 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Daily Weight'" do
-      visit root_path
-      expect(page).to have_content('Daily Weight')
-    end
-
-    it "should have the title" do
-      visit root_path
-      expect(page).to have_title("Daily Weight")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title(' | Home')
-    end
+    it { should have_content('Daily Weight') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title(' | Home') }
   end
 end
